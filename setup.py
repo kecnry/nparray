7092f18f0f6ed2b6af33e0906cadf7640848cd67
Kyle Conroy <kyleconroy@gmail.com>
2018-10-29 12:48:44 -0400

    update versioning for 1.0.0 release

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 from distutils.core import setup
 
 setup(name='nparray',
-      version='0.1.0',
+      version='1.0.0',
       description='Numpy Array Helpers',
       author='Kyle Conroy',
       author_email='kyleconroy@gmail.com',
       url='https://www.github.com/kecnry/nparray',
+      download_url = 'https://github.com/kecnry/nparray/tarball/1.0.0',
       packages=['nparray'],
      )
